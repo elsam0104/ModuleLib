8b698dd8ac17de3dd694b76e991deaf7b1d43812
YeEun Oh <77712364+elsam0104@users.noreply.github.com>
2022-07-11 14:38:46 +0900

    사운드 모듈 업로드
    오디오 믹서를 사용하여 슬라이더로 음량 조절, 효과음과 배경음을 실행시키는 함수

---
+++ SoundType.cs
@@ -0,0 +1,17 @@
+namespace SoundType
+{
+    public enum EffType
+    {
+        //eff name here
+        Example = 0,
+    }
+    public enum BgmType
+    {
+        //bgm name here
+        Example = 0,
+
+    }
+    public class SoundType
+    {
+    }
+}
